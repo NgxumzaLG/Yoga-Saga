5ab5d0ec9e23b9d21bde6461a44525bde2be571b
Peggymailula <peggymailula@gmail.com>
2021-11-30 16:16:56 +0200

    teachable machine

---
+++ public/yogaList.js
@@ -0,0 +1,35 @@
+var poseList = [
+    {
+        image:"./images/chairPose.jpg",
+        name: 'chair pose'
+    },
+    {
+        image:"./images/disko.jpg",
+        name:'disko pose'
+    },
+    {
+        image:"./images/bendingTree.png",
+        name:'bending tree'
+    },
+    {
+        image:"./images/MJ.jpg",
+        name: 'Michael Jackson'
+
+    },
+    {
+        image:"./images/triangleForward.jpg",
+        name:'triangle forward'
+    },
+    {
+        image:"./images/warrior_pose.jpg",
+        name:'warrior pose'
+    },
+    {
+        image:"./images/yogaTreePose.jpg",
+        name: 'tree pose'
+    },
+    {
+        image:"./images/stand.jpeg",
+        name: 'tree pose'
+    }
+];
