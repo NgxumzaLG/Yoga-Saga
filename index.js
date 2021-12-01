60a27c8175b205b0f7dbef3cb28250c912e43845
ishmael-sikhikhi <i.sikhikhi@gmail.com>
2021-12-01 12:33:05 +0200

    gaming screen added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
+const { engine } = require('express-handlebars');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 
@@ -11,8 +11,8 @@ app.use(session({
     saveUninitialized: true
 }));
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
 
 app.use(express.static('public'));
 
